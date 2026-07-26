53de5c8f8948e564e3849523f21411f6c384d629
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-07-26 09:07:26 +0530

    CRUD OPERATIONS

---
+++ students.py
@@ -0,0 +1,14 @@
+def insert_student(adm, first, last, roll, mobile):
+    pass
+
+
+def delete_student(adm):
+    pass
+
+
+def update_student(adm, field, value):
+    pass
+
+
+def view_students():
+    pass
