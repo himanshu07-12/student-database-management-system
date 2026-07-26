fc7163e23f117451c2990b110f6400b17aba150b
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-07-26 08:56:44 +0530

    CREATED DATABASE CONNECTION

---
+++ database.py
@@ -0,0 +1,10 @@
+import mysql.connector
+from config import HOST, USER, PASSWORD, DATABASE
+
+def get_connection():
+    return mysql.connector.connect(
+        host=HOST,
+        user=USER,
+        password=PASSWORD,
+        database=DATABASE
+    )
