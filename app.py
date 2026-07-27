b3b7578020836e18b8b51e52a269ce892c864f73
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-07-27 06:44:05 +0530

    test program of crud

---
+++ app.py
@@ -0,0 +1,39 @@
+#to insert a student into the database
+"""from students import insert_student
+
+success, message = insert_student(
+    103,
+    "Divyanshu",
+    "Tripathi",
+    12,
+    7355525494
+)
+
+print(success)
+print(message)"""
+# to view all students in the database
+"""from students import view_students
+
+students = view_students()
+
+for student in students:
+    print(student)
+"""     
+#to delete a student in the database
+"""from students import delete_student
+
+success, message = delete_student(101)
+
+print(success)
+print(message)"""
+#to update a student in the database
+"""from students import update_student
+
+success, message = update_student(
+    102,
+    "fname",
+    "Vaishnavi"
+)
+
+print(success)
+print(message)"""
