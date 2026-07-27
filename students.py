9852a591aa8df81efed963af3780c62dd9c82377
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-07-27 06:29:37 +0530

    ADDED UPDATE FUNCTION

--- students.py
+++ students.py
@@ -66,7 +66,67 @@ def delete_student(adm):
 
 
 def update_student(adm, field, value):
-    pass
+
+    mydb = get_connection()
+    mycursor = mydb.cursor()
+
+    query = "SELECT admno FROM student WHERE admno=%s"
+    mycursor.execute(query, (adm,))
+    student = mycursor.fetchone()
+
+    if not student:
+        mycursor.close()
+        mydb.close()
+        return False, "Student not found"
+
+    allowed_fields = [
+        "admno",
+        "fname",
+        "lname",
+        "roll",
+        "mobno"
+    ]
+
+    if field not in allowed_fields:
+        mycursor.close()
+        mydb.close()
+        return False, "Invalid field"
+
+    if field == "mobno":
+        if len(str(value)) != 10:
+            mycursor.close()
+            mydb.close()
+            return False, "Invalid Mobile Number"
+
+    if field == "roll":
+        query = "SELECT roll FROM student WHERE roll=%s"
+        mycursor.execute(query, (value,))
+        if mycursor.fetchone():
+            mycursor.close()
+            mydb.close()
+            return False, "Roll Number already exists"
+
+    if field == "admno":
+        query = "SELECT admno FROM student WHERE admno=%s"
+        mycursor.execute(query, (value,))
+        if mycursor.fetchone():
+            mycursor.close()
+            mydb.close()
+            return False, "Admission Number already exists"
+
+    query = f"""
+    UPDATE student
+    SET {field}=%s
+    WHERE admno=%s
+    """
+
+    mycursor.execute(query, (value, adm))
+    mydb.commit()
+
+    mycursor.close()
+    mydb.close()
+
+    return True, "Student Updated Successfully"
 
 
 def view_students():
