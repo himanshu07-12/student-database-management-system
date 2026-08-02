782a6657ba8863aa9c5313e0f1080cd53282fda8
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-08-02 23:33:52 +0530

    disabling browser cache

--- app.py
+++ app.py
@@ -202,5 +202,14 @@ def export_students():
 
     return response
 
+@app.after_request
+def add_header(response):
+
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+
+    return response
+
 if __name__ == "__main__":
     app.run(debug=True)
