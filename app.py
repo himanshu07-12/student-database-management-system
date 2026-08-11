8c613be11d07cc1b8a92d13ab659cf61783c3b78
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-08-11 11:43:03 +0530

    error handling complete

--- app.py
+++ app.py
@@ -231,5 +231,15 @@ def download_resume():
         as_attachment=True
     )
 
+@app.errorhandler(404)
+def page_not_found(error):
+
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+
+    return render_template("500.html"), 500
+
 if __name__ == "__main__":
     app.run()
