336eb58fc75bdd9a00f4396b6e802a3f03cc29ac
HIMANSHU TRIPATHI <tripathihimanshu694@gmail.com>
2026-08-11 11:53:11 +0530

    database env

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-HOST = "localhost"
-USER = "seldom"
-PASSWORD = "him123"
-DATABASE = "project"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST = os.getenv("DB_HOST")
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+DATABASE = os.getenv("DB_NAME")
